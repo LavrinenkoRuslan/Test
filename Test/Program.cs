5b2035c625975769458a503fd7f4c8ec4187de84
Lavrinenko Ruslan <LavrinenkoRuslanNik@gmail.com>
2024-12-21 19:47:41 +0200

    test

---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using Test;
+
+MessagePackSerializerOptions GetResolver()
+{
+    var resolver = CompositeResolver.Create(
+        NativeDecimalResolver.Instance,
+        NativeGuidResolver.Instance,
+        NativeDateTimeResolver.Instance,
+        TypelessObjectResolver.Instance,
+        StandardResolver.Instance
+    );
+    return MessagePackSerializerOptions.Standard.WithResolver(resolver).WithOmitAssemblyVersion(true);
+}
+
+
+Dictionary<string, IPrimaryKeyItem> itemList = new Dictionary<string, IPrimaryKeyItem>();
+
+itemList.Add("1", new PrimaryKeyItem<int>(1));
+itemList.Add("2", new PrimaryKeyItem<string>("2"));
+
+var json = MessagePackSerializer.SerializeToJson(itemList, GetResolver());
+
+Console.Read();
